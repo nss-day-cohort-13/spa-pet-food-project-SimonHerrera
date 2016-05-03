e43b3f4f678140c4b990b275a0c4e9d11e677208
Simon Herrera <simonherrera.tech@gmail.com>
2016-05-03 09:54:46 -0500

    Files Updated

--- js/food.js
+++ js/food.js
@@ -1,66 +1,60 @@
-{
-  "dog_brands": [
-    {
-      "name": "Chuck wagon",
-      "types": [
-        {
-          "type": "all_natural",
-          "volumes": [
-            {
-              "name": "32oz",
-              "price": 9.99
-            },
-            {
-              "name": "64oz",
-              "price": 17.99
-            }
-          ]
-        },
-        {
-          "type": "standard",
-          "volumes": [
-            {
-              "name": "58oz",
-              "price": 12.99
-            },
-            {
-              "name": "72oz",
-              "price": 21.99
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "name": "Purina",
-      "types": [
-        {
-          "type": "puppy",
-          "volumes": [
-            {
-              "name": "16oz",
-              "price": 8.99
-            },
-            {
-              "name": "24oz",
-              "price": 14.99
-            }
-          ]
-        },
-        {
-          "type": "standard",
-          "volumes": [
-            {
-              "name": "58oz",
-              "price": 19.99
-            },
-            {
-              "name": "72oz",
-              "price": 24.99
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", 'dog_brands.json', true);
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4) {
+    //404 could not find
+    //500 error
+    //200-299 ok or 304 no change since last request
+    var status = xhr.status;
+    if ((status >= 200 && status < 300) ||
+      status === 304) {
+        console.log("status", status);
+        // var data = JSON.parse(this.responseText);
+        // console.log("data", data);
+        document.getElementById('display-pet-food')
+        .innerHTML = xhr.responseText;
+        alert(xhr.responseText);
+      } else {
+          alert('failure/error');
+      }
+  }
+};
+
+xhr.send(null);
+
+// alert(xhr.responseText + "This is outside of the event handler");
+
+console.log("show data", xhr );
+
+
+//Example of for loop instead of forEach from trial page
+
+  //   for(i = 0; i < data.dog_brands.length; i += 1) {
+  //     var dogFood = data.dog_brands[i];
+  //     foodList.innerHTML +=
+  //     "<table><tr>" + dogFood.name + "</tr></table>";
+
+  //     for(j = 0; j < dogFood.types.length; j += 1) {
+  //     var dogFoodType = dogFood.types[j];
+  //     foodList.innerHTML +=
+  //     "<table><tr>" + dogFoodType.type + "</tr></table>";
+
+  //         for(k = 0; k < dogFoodType.volumes.length; k += 1 ) {
+  //         var volumes = dogFoodType.volumes[k];
+  //         foodList.innerHTML +=
+  //         "<table><tr>" + volumes.name + "</tr></table>";
+
+  //         foodList.innerHTML +=
+  //         "<table><tr>" + volumes.price + "</tr></table>";
+
+  //         }
+  //     }
+  // }
+
+
+
+
+
+
